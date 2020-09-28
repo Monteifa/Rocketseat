96d1a50a351014f9a34518e879949fd200be9ed4
Monteifa <fabiopmont@gmail.com>
2020-09-28 15:32:08 -0300

    Primeiro commit

---
+++ Twitch Clone/src/components/ChannelList/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import {
+  List,
+  ChannelContainer,
+  LeftSide,
+  Avatar,
+  Column,
+  Username,
+  Info,
+  RightSide,
+  WhiteCircle,
+} from './styles';
+
+const ChannelList: React.FC = ({ children }) => {
+  const ChannelItem = () => (
+    <ChannelContainer>
+      <LeftSide>
+        <Avatar />
+        <Column>
+          <Username>Monteifa</Username>
+          <Info>36 new videos</Info>
+        </Column>
+      </LeftSide>
+
+      <RightSide>
+        <WhiteCircle />
+      </RightSide>
+    </ChannelContainer>
+  );
+  return (
+    <List>
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+    </List>
+  );
+};
+
+export default ChannelList;
---
+++ Twitch Clone/src/components/StreamList/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import streamThumbnail from '../../images/stream_thumbnail.jpg';
+
+import {
+  List,
+  StreamContainer,
+  StreamRow,
+  StreamThumbnail,
+  StreamColumn,
+  StreamHeader,
+  StreamAvatar,
+  StreamUsername,
+  StreamDescription,
+  StreamCategory,
+  TagRow,
+  TagView,
+  TagText,
+} from './styles';
+
+const StreamList: React.FC = ({ children }) => {
+  const StreamItem = () => (
+    <StreamContainer>
+      <StreamThumbnail source={streamThumbnail} />
+      <StreamColumn>
+        <StreamRow>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUsername numberOfLines={1}>Monteifa</StreamUsername>
+          </StreamHeader>
+
+          <StreamDescription numberOfLines={1}>
+            fagsdgnsmgnsmgsnmgshgnjshgjshgjs
+          </StreamDescription>
+
+          <StreamCategory numberOfLines={1}>Sgdjagdagjdagha</StreamCategory>
+        </StreamRow>
+
+        <TagRow>
+          <TagView>
+            <TagText>Portuguese</TagText>
+          </TagView>
+          <TagView>
+            <TagText>Web Development</TagText>
+          </TagView>
+        </TagRow>
+      </StreamColumn>
+    </StreamContainer>
+  );
+  return (
+    <List>
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+    </List>
+  );
+};
+
+export default StreamList;
